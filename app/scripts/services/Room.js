05e7474be15a8dccceca8d7a34126d65fa0c0a82
Luna Kang <luna.kang.ca@gmail.com>
2016-09-21 12:26:09 -0700

    List Chat Rooms V1

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,11 +1,13 @@
 (function() {
   function Room($firebaseArray) {
+    var Room = {};
+      
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
-
-    return {
-      all: rooms
-    };
+      
+    Room.all = rooms;
+      
+    return Room;
   }
 
   angular
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -7,14 +7,14 @@
             });
         
         $stateProvider
-            .state('room', {
+            .state('home', {
                 url: '/',
-                controller: 'RoomCtrl as room',
-                templateUrl: '/templates/room.html'
+                controller: 'HomeCtrl as home',
+                templateUrl: '/templates/home.html'
             });
     }
     
-angular
-    .module('blocChat', ['ui.router'], ['firebase'])
-    .config(config);
+    angular
+        .module('blocChat', ['ui.router', 'firebase'])
+        .config(config);
 })();
